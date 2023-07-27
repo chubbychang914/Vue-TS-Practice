1a67795ef1ba11b2a2200ba2c8f43bdd1d4309c2
Johnny Chang <johnny22375@gmail.com>
2023-07-27 17:51:38 +0800

    successfully rendered the whole list

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import Test1 from '@/components/test/Test1.vue'
+import { Job } from 'src/types/Job'
+import { ref } from 'vue';
+
+const jobList = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+])
 </script>
 
 <template>
-  <Test1 />
+  <div id="HomeView">
+    <h1 v-for="item in jobList" :key="item.id">{{ item.title }}</h1>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 #HomeView {}
-
-.center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 </style>
