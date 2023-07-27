377869c479e5e64c2a7b62481f2a6b721410b117
Johnny Chang <johnny22375@gmail.com>
2023-07-27 16:57:08 +0800

    made a test 1 component

---
+++ src/components/test/test1.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <div id="test1">
+    <h1>This is test 1</h1>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+#test1{
+
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,36 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue';
+
+// using reactive ====================
+// const state = reactive({
+//   name: 'Johnny', // will automatically inferred to be a string
+//   age: 25 as number | string
+// })
+// const changeName = (newName: string) => state.name = newName
+// const changeAge = (newAge: number | string) => state.age = newAge
+
+// using refs ====================
+const name = ref('Johnny Chang')
+const age = ref<number | string>(26)
+
+const changeName = (newName: string) => name.value = newName
+const changeAge = (newAge: number | string) => age.value = newAge
+
 </script>
 
 <template>
   <div id="HomeView">
-    <h1>This is the home view page</h1>
+    <!-- ========== using reactive ========== -->
+
+    <!-- <h1>{{ state.name }} -- {{ state.age }}</h1>
+    <button @click="changeName('Joby')">Change Name</button>
+    <button @click="changeAge(30)">Change Age</button> -->
+
+    <!-- ========== using refs ========== -->
+    <h1>{{ name }} -- {{ age }}</h1>
+    <button @click="changeName('Hello world')">Ref Change Name</button>
+    <button @click="changeAge('123')">Ref Change Age</button>
+
   </div>
 </template>
 
