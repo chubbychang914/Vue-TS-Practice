6072a6a31d133c27ab458bc69dca9e90fda0f29b
Johnny Chang <johnny22375@gmail.com>
2023-07-27 17:57:10 +0800

    moved data to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
+
+import { Job } from '@/types/Job';
+
+const jobList = ref<Job[]>([
+  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
+  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
+  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
+  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
+  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
+])
+
 </script>
 
 <template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,8 @@
 <script setup lang="ts">
-import { Job } from 'src/types/Job'
-import { ref } from 'vue';
-
-const jobList = ref<Job[]>([
-  { title: 'farm worker', location: 'lon lon ranch', salary: 30000, id: '1' },
-  { title: 'quarryman', location: 'death mountain', salary: 40000, id: '2' },
-  { title: 'flute player', location: 'the lost woods', salary: 35000, id: '3' },
-  { title: 'fisherman', location: 'lake hylia', salary: 21000, id: '4' },
-  { title: 'prison guard', location: 'gerudo valley', salary: 32000, id: '5' }
-])
 </script>
 
 <template>
   <div id="HomeView">
-    <h1 v-for="item in jobList" :key="item.id">{{ item.title }}</h1>
   </div>
 </template>
 
